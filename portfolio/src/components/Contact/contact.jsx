98eeda09bd4a3266f77d7f5ea958fad201da57ad
Steven Canivet <canivet.steven@gmail.com>
2024-08-09 13:40:25 +0200

    email

--- portfolio/src/components/Contact/contact.jsx
+++ portfolio/src/components/Contact/contact.jsx
@@ -4,7 +4,66 @@ import { ReactComponent as Phone } from '../../assets/phone.svg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 
+import React, { useEffect, useState } from 'react'
+import emailjs from '@emailjs/browser'
+
 const Contact = () => {
+  const [formState, setFormState] = useState({
+    name: '',
+    firstName: '',
+    subject: '',
+    email: '',
+    message: '',
+  })
+
+  const [confirm, setConfirm] = useState('')
+  const [confirmVisible, setConfirmVisible] = useState(false)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormState({
+      ...formState,
+      [name]: value,
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .send(
+        'service_portfolio', // Remplace par ton Service ID
+        'template_portfolio', // Remplace par ton Template ID
+        formState,
+        'QeBm7uGs-Rbgo1Hq7' // Remplace par ton User ID
+      )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text)
+        setFormState({
+          name: '',
+          firstName: '',
+          subject: '',
+          email: '',
+          message: '',
+        })
+        setConfirm('Le message à bien été envoyé')
+        setConfirmVisible(true)
+        setTimeout(() => {
+          setConfirmVisible(false)
+        }, 10000)
+      })
+      .catch((err) => {
+        console.error('FAILED...', err)
+        setConfirm(
+          `En raison d'une erreur le message ne s'est pas envoyé correctement`
+        )
+        setConfirmVisible(true)
+        setTimeout(() => {
+          setConfirmVisible(false)
+        }, 10000)
+      })
+  }
+
   return (
     <section className="contact padding" id="contact">
       <h2 className="underline">CONTACT ME</h2>
@@ -19,35 +78,76 @@ const Contact = () => {
             <p>: +33 06 47 01 30 99</p>
           </div>
         </div>
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <div className="wrapper">
             <div className="wrapper_container">
               <div className="wrapper_container_inputs">
                 <label className="label"></label>
-                <input className="input" type="text" placeholder="Name" />
+                <input
+                  className="input"
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  value={formState.name}
+                  onChange={handleChange}
+                  required
+                />
               </div>
               <div className="wrapper_container_inputs">
                 <label className="label"></label>
-                <input className="input" type="text" placeholder="Firstname" />
+                <input
+                  className="input"
+                  type="text"
+                  name="firstName"
+                  placeholder="Firstname"
+                  value={formState.firstName}
+                  onChange={handleChange}
+                  required
+                />
               </div>
               <div className="wrapper_container_inputs">
                 <label className="label"></label>
-                <input className="input" type="text" placeholder="Object" />
+                <input
+                  className="input"
+                  type="text"
+                  name="subject"
+                  placeholder="Subject"
+                  value={formState.subject}
+                  onChange={handleChange}
+                  required
+                />
               </div>
               <div className="wrapper_container_inputs">
                 <label className="label"></label>
-                <input className="input" type="text" placeholder="Email" />
+                <input
+                  className="input"
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={formState.email}
+                  onChange={handleChange}
+                  required
+                />
               </div>
             </div>
             <div className="wrapper_message">
               <label className="label"></label>
-              <textarea className="textarea" />
+              <textarea
+                className="textarea"
+                name="message"
+                value={formState.message}
+                onChange={handleChange}
+                required
+              />
             </div>
           </div>
-          <button className="button">
+          <button className="button" type="submit">
             <FontAwesomeIcon icon={faPaperPlane} className="button_icon" />
           </button>
         </form>
+        <div>
+          <p>{confirmVisible && confirm}</p>
+        </div>
       </div>
     </section>
   )
